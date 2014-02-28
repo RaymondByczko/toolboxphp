3a8d52ce74e6a572659e3d6afd09de38fc6cfc68
Raymond Byczko <raymondbyczko@att.net>
2014-02-27 20:45:20 -0500

    self-RByczko; 2014-02-27 Feb 27; Enhanced test harness test02.php

--- importprocess/lib2/CFSImport.php
+++ importprocess/lib2/CFSImport.php
@@ -13,9 +13,14 @@
   * Further, tokens, like a dash can be inserted at any point to create
   * the new virtual column.
   * @start_date 2014-02-26 Feb 26
+  * @change_history 2014-02-27 Feb 27; RByczko; Added employee_start_data,
+  * employee_location_data. Fixed a number of bugs (incorrect names for
+  * member variables), fixed throw call, fixed logic for m_EmployeeS and
+  * for m_EmployeeL.
   * @status Incomplete - not all desired metrics (quantity) are calculated.
-  * Further, a few additional methods have to be added to provide service
-  * to the client code.  Lastly, a buffer_fgetcsv needs to be written,
+  * Further, although additional methods have been added to provide service
+  * to the client code, a way of resetting an object can be implemented so
+  * that a new file can be imported. Lastly, a buffer_fgetcsv needs to be written,
   * and abstracted to a base class, so we don't read 1 line at a time,
   * while still conserving memory.
   */
@@ -59,90 +64,101 @@ class CFSImport
 	}
 	public function set_expectedfields($ef)
 	{
-		$this->expectedfields = $ef;
+		$this->m_expectedfields = $ef;
+	}
+	public function get_EmployeeS()
+	{
+		return $this->m_EmployeeS;
+	}
+	public function get_EmployeeL()
+	{
+		return $this->m_EmployeeL;
 	}
 	public function init_transformation($key)
 	{
-		$texists = array_key_exists($key, $this->transformations);
-		if (!texists)
+		$texists = array_key_exists($key, $this->m_transformations);
+		if (!$texists)
 		{
-			$this->transformations[$key] = array();
-			$this->transformations[$key][0] = array('status'=>'open');
+			$this->m_transformations[$key] = array();
+			$this->m_transformations[$key][0] = array('status'=>'open');
 		}
 	}
 	public function add_transformation_c($key, $column, $start, $end)
 	{
-		$texists = array_key_exists($key, $this->transformations);
-		if (texists)
+		$texists = array_key_exists($key, $this->m_transformations);
+		if ($texists)
 		{
 			// @TODO test to determine if status is open.
-			$this->transformations[$key][] = array('type'=>'c','column'=>$column, 'start'=>$start, 'end'=>$end);
+			$this->m_transformations[$key][] = array('type'=>'c','column'=>$column, 'start'=>$start, 'end'=>$end);
 		}
 		else
 		{
-			throw Exception('initialize tranformation with init_transformation');
+			throw new Exception('initialize tranformation with init_transformation');
 		}
 	}
 	public function add_transformation_l($key, $column, $left)
 	{
-		$texists = array_key_exists($key, $this->transformations);
-		if (texists)
+		$texists = array_key_exists($key, $this->m_transformations);
+		if ($texists)
 		{
 			// @TODO test to determine if status is open.
-			$this->transformations[$key][] = array('type'=>'l','column'=>$column, 'left'=>$left);
+			$this->m_transformations[$key][] = array('type'=>'l','column'=>$column, 'left'=>$left);
 		}
 		else
 		{
-			throw Exception('initialize tranformation with init_transformation');
+			throw new Exception('initialize tranformation with init_transformation');
 		}
 	}
 	public function add_transformation_t($key, $token)
 	{
-		$texists = array_key_exists($key, $this->transformations);
-		if (texists)
+		$texists = array_key_exists($key, $this->m_transformations);
+		if ($texists)
 		{
 			// @TODO test to determine if status is open.
-			$this->transformations[$key][] = array('type'=>'t','token'=>$token);
+			$this->m_transformations[$key][] = array('type'=>'t','token'=>$token);
 		}
 		else
 		{
-			throw Exception('initialize tranformation with init_transformation');
+			throw new Exception('initialize tranformation with init_transformation');
 		}
 	}
 	public function close_transformation($key)
 	{
-		$texists = array_key_exists($key, $this->transformations);
-		if (texists)
+		$texists = array_key_exists($key, $this->m_transformations);
+		if ($texists)
 		{
-			$this->transformations[$key][0] = array('status'=>'closed');
+			$this->m_transformations[$key][0] = array('status'=>'closed');
 		}
 		else
 		{
-			throw Exception('initialize tranformation with init_transformation');
+			throw new Exception('initialize tranformation with init_transformation');
 		}
 	}
 	public function process_file()
 	{
+		syslog(LOG_DEBUG,'process_file');
 		$row = 1;
 		// Open csv file.
 		if (($handle = fopen($this->m_filename, "r")) !== FALSE)
 		{
 			// Acquire the header.
-			if ($data = fgetcsv($handle, 1, ",")) !== FALSE)
+			if (($data = fgetcsv($handle)) !== FALSE)
 			{
 				
 				$num_afields = count($data);
 				$num_efields = count($this->m_expectedfields); 
 				if ($num_afields != $num_efields)
 				{
-					throw Exception('num_afields='.$num_afields.';num_efields='.$num_efields);
+					throw new Exception('num_afields='.$num_afields.';num_efields='.$num_efields);
 				}
 			}
 			// Process each line
 			while (($data = fgetcsv($handle)) !== FALSE) {
+				syslog(LOG_DEBUG,'processing a line');
 				$row++;
 				if (count($data) != count($this->m_expectedfields))
 				{
+					syslog(LOG_DEBUG,'...error detected');
 					$this->m_errorList[] = array('line'=>$row, 'errorFound'=>'number of fields not correct', 'ignored'=>true);				
 					continue;
 				}
@@ -152,14 +168,26 @@ class CFSImport
 				$Location = $data[3];
 				$SKU = $data[4];
 				$Quantity = $data[5];
+				syslog(LOG_DEBUG,'...Employee='.$Employee);
 				// Store Location per Employee.
 				if (array_key_exists($Employee,$this->m_EmployeeL))
 				{
 					$this->m_EmployeeL[$Employee][] = $Location;
 				}
+				else
+				{
+					$this->m_EmployeeL[$Employee] = array();
+					$this->m_EmployeeL[$Employee][] = $Location;
+				}
 				// Store StartTime per Employee.
 				if (array_key_exists($Employee,$this->m_EmployeeS))
 				{
+
+					$this->m_EmployeeS[$Employee][] = $StartTime;
+				}
+				else
+				{
+					$this->m_EmployeeS[$Employee] = array();
 					$this->m_EmployeeS[$Employee][] = $StartTime;
 				}
 				// Note: Above Store operations insure aligned order
@@ -168,12 +196,50 @@ class CFSImport
 			}
 			fclose($handle);
 			// Sort the StartTime component and have Location follow.
-			foreach ($this->mEmployeeS as $curEmployee=>$curEmployeeStart)
+			foreach ($this->m_EmployeeS as $curEmployee=>$curEmployeeStart)
 			{
-				array_multisort($this->m_EmployeeS($curEmployee), $this->m_EmployeeL($curEmployee));
+				array_multisort($this->m_EmployeeS[$curEmployee], $this->m_EmployeeL[$curEmployee]);
 			}
 		}
 		
+	}
+	/**
+	  * employee_start_data: simply prints out info related to employee start data.
+	  * This can also be used as an example of how to use the result of get_EmployeeS.
+	  */
+	public function employee_start_data()
+	{
+		echo '<p>';
+		echo '<p>EMPLOYEE START DATA';
+		$es = $this->get_EmployeeS();
+		foreach ($es as $key_Employee=>$value_StartTimeArray)
+		{
+			echo '<p>Employee='.$key_Employee;
+			foreach ($value_StartTimeArray as $key=>$value_StartTime)
+			{
+				echo '<p>...'.$value_StartTime;
+			}
+		}
+		echo '<p>';
+	}
+	/**
+	  * employee_location_data: simply prints out info related to employee location data.
+	  * This can also be used as an example of how to use the result of get_EmployeeL.
+	  */
+	public function employee_location_data()
+	{
+		echo '<p>';
+		echo '<p>EMPLOYEE LOCATION DATA';
+		$el = $this->get_EmployeeL();
+		foreach ($el as $key_Employee=>$value_LocationArray)
+		{
+			echo '<p>Employee='.$key_Employee;
+			foreach ($value_LocationArray as $key=>$value_Location)
+			{
+				echo '<p>...'.$value_Location;
+			}
+		}
+		echo '<p>';
 	}
 }
 ?>
--- importprocess/test/test02.php
+++ importprocess/test/test02.php
@@ -4,11 +4,18 @@
   * @company self
   * @author Raymond Byczko
   * @purpose To provide a test harness for CFSImport.
-  * @status Incomplete - this code has to be run, but it should be close.
+  * @status Incomplete - this code runs. The assignment
+  * of $filename needs to be researched.
   */
 
-require_once 'lib/CFSImport.php';
-$filename = './mydata.csv';
+require_once 'lib2/CFSImport.php';
+try {
+
+// This kind of works.
+$filename = '../data/small_5.csv';
+
+/// $filename = 'data/small_5.csv';
+/// $filename = '/data/small_5.csv';
 $cfs = new CFSImport($filename);
 $ef = array('Start Time', 'End Time', 'Employee', 'Location', 'SKU', 'Quantity');
 $cfs->set_expectedfields($ef);
@@ -22,4 +29,34 @@ $cfs->add_transformation_t('department', '-');
 $cfs->close_transformation('department');
 
 $cfs->process_file();
+$es = $cfs->get_EmployeeS();
+$el = $cfs->get_EmployeeL();
+echo '<p>--es--<p>'."\n";
+var_dump($es);
+echo "\n\n";
+echo '<p>--el--<p>'."\n";
+var_dump($el);
+echo "\n\n";
+echo '<p>EMPLOYEE START DATA';
+foreach ($es as $key_Employee=>$value_StartTimeArray)
+{
+	echo '<p>Employee='.$key_Employee;
+	// echo '<p>...';
+	foreach ($value_StartTimeArray as $key=>$value_StartTime)
+	{
+		echo '<p>...'.$value_StartTime;
+	}
+}
+$cfs->employee_start_data();
+$cfs->employee_location_data();
+echo '<p>test02.php-success'."\n";
+} catch (Exception $eobj)
+{
+	$msg = $eobj->getMessage();
+	echo $msg."\n";
+	$tr = $eobj->getTrace();
+	var_dump($tr);
+
+}
+echo '<p>test02.php-success2'."\n";
 ?>
