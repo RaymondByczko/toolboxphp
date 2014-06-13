5d5617b607278f740bb24d73f46ef8c8fc562677
Raymond Byczko <raymondbyczko@att.net>
2014-06-13 15:48:31 -0400

    self-RByczko; 2014-06-13; Add comment for main.php - project euler problem 19.

--- projecteuler/19/main.php
+++ projecteuler/19/main.php
@@ -6,6 +6,13 @@
  * @file 19/countingsundays/main.php
  */
 
+
+// EXPLANATION FOR FOLLOWING CODE FRAGMENT...
+// JUST MAKING SURE I CAN DEBUG INTO THIS WITH NETBEANS.
+// SOMETIMES IT DOESN'T COOPERATE AND NEED TO LOOK AT XDEBUG
+// AND OTHER MATTERS
+// @todo remove this
+//
 function add_this($a, $b)
 {
     $res = $a + $b;
